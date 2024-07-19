878eb88c281119c5d4271dcbbae6fec459176fa1
notEven1day <iliyanpopov14.09.2006@gmail.com>
2024-07-20 00:59:28 +0300

    Update ShopDbContext.cs

--- ShopDbContext.cs
+++ ShopDbContext.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BackEndFixage.Models;
 
-namespace BackEndFixage
+namespace BackEndFixage.Data
 {
-    internal class ShopDbContext
+    public class ShopDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure User entity
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<Cart>()
+                .HasOne(c => c.User)
+                .WithOne(u => u.Cart)
+                .HasForeignKey<Cart>(c => c.UserId);
+
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Cart)
+                .WithMany()
+                .HasForeignKey(o => o.CartId);
+
+            modelBuilder.Entity<CartItem>()
+                .HasOne(ci => ci.Cart)
+                .WithMany(c => c.CartItems)
+                .HasForeignKey(ci => ci.CartId);
+
+            modelBuilder.Entity<CartItem>()
+                .HasOne(ci => ci.Product)
+                .WithMany()
+                .HasForeignKey(ci => ci.ProductId);
+
+            // Configure Product entity with enums
+            modelBuilder.Entity<Product>()
+                .Property(p => p.RoomCategory)
+                .HasConversion<int>();
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.FurnitureTypeCategory)
+                .HasConversion<int>();
+        }
     }
 }
