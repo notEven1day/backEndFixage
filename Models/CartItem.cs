711f341505430d3fcb0b821ccd44e4a409c29b13
notEven1day <iliyanpopov14.09.2006@gmail.com>
2024-07-20 00:56:38 +0300

    Update CartItem.cs

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BackEndFixage
+public class CartItem
 {
-    internal class CartItem
+    [Key]
+    public int CartItemId { get; set; }
+
+    [Required]
+    public int CartId { get; set; }
+
+    [ForeignKey("CartId")]
+    public virtual Cart Cart { get; set; }
+
+    [Required]
+    public int ProductId { get; set; }
+
+    [ForeignKey("ProductId")]
+    public virtual Product Product { get; set; }
+
+    [Required]
+    public int Quantity { get; set; }
+
+    public CartItem()
     {
     }
 }
