4b89894b4e6c78967323a6a6bfb8db383b558776
notEven1day <iliyanpopov14.09.2006@gmail.com>
2024-07-20 00:57:22 +0300

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,44 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BackEndFixage
+public class User
 {
-    internal class User
+    [Key]
+    public int UserId { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Username { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Password { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(100)]
+    public string Email { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string LastName { get; set; }
+
+    [MaxLength(20)]
+    public string PhoneNumber { get; set; }
+
+    [MaxLength(255)]  
+    public string ProfilePicUrl { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; }
+    public virtual Cart Cart { get; set; }
+
+    public User()
     {
+        Orders = new HashSet<Order>();
     }
 }
