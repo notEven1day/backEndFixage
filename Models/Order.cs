3497cdfe7fe3f8a0b509dd8764f19844ceaac902
notEven1day <iliyanpopov14.09.2006@gmail.com>
2024-07-20 00:56:57 +0300

    Update Order.cs

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BackEndFixage
+public class Order
 {
-    internal class Order
+    [Key]
+    public int OrderId { get; set; }
+
+    [Required]
+    public int UserId { get; set; }
+
+    [ForeignKey("UserId")]
+    public virtual User User { get; set; }
+
+    [Required]
+    public int CartId { get; set; }
+
+    [ForeignKey("CartId")]
+    public virtual Cart Cart { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal DeliveryPrice { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string Address { get; set; }
+
+    [Required]
+    public DateTime OrderDate { get; set; }
+
+    public Order()
     {
     }
 }
